446fd93502a31baf47108e0a82d2b8e676700954
toutouff <63313022+toutouff@users.noreply.github.com>
2024-04-19 11:09:14 +0200

    ajout route post

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,3 +10,7 @@ app.get("/", (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`[Server]: I am running at http://localhost:${port}`);
 });
+
+app.post("/signUp", (req: Request, res: Response) => {
+  console.log("[Server]: i have recieve a request");
+});
