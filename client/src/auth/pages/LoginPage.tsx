6e8966f0dd4c71dae152c5e0bbb8c018d28bde4b
Paul MAZEAU <mazeau.paul@gmail.com>
2024-04-19 16:39:56 +0200

    minor style fixed

--- client/src/auth/pages/LoginPage.tsx
+++ client/src/auth/pages/LoginPage.tsx
@@ -4,7 +4,7 @@ const LoginPage = () => {
     <>
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a href="#" className="flex items-center mb-4 text-2xl font-semibold text-gray-900 dark:text-white">
+        <a href="#" className="flex items-center mb-4,text-2xl font-semibold text-gray-900 dark:text-white">
           <img className="w-40 h-auto mr-2" src="./svg/logo.svg" alt="logo" />
         </a>
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
@@ -32,7 +32,7 @@ const LoginPage = () => {
                 </div>
                 <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
               </div>
-              <button type="submit" className="w-full text-white bg-sky-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+              <button type="submit" className="w-full text-white bg-primary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Don’t have an account yet? <a href="/signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
               </p>
