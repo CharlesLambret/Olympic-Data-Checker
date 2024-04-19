7524fcd1acaeddfa0d10545d37e5544befc22d49
Charles Lambret <charleslambretpro@gmail.com>
2024-04-19 16:55:42 +0200

    route read solo fonctionnelle

--- backend/src/CRUD/users/classicuser/getuser.ts
+++ backend/src/CRUD/users/classicuser/getuser.ts
@@ -1,20 +1,36 @@
 import { MongoConnection } from '../../../db/call';
 import { ObjectId } from 'mongodb';
 
-export async function getUser(id: string) {
+export async function getUserById(id: string) {
     const client = await MongoConnection();
     const db = client.db("TP-React"); 
     const users = db.collection("users");
 
     try {
-        const foundUsers = await users.find({ _id: new ObjectId(id), isAdmin: false }).toArray();
-        if (foundUsers.length === 0) {
+        const foundUser = await users.findOne({ _id: new ObjectId(id), isAdmin: false });
+        if (!foundUser) {
             return "User not found.";
         }
 
-        return foundUsers;
+        return foundUser;
     } catch (error) {
         console.error("Failed to get user:", error);
+        throw error; 
+    } finally {
+        await client.close(); 
+    }
+}
+
+export async function getUsers() {
+    const client = await MongoConnection();
+    const db = client.db("TP-React"); 
+    const users = db.collection("users");
+
+    try {
+        const foundUsers = await users.find({ isAdmin: false }).toArray();
+        return foundUsers;
+    } catch (error) {
+        console.error("Failed to get users:", error);
         throw error; 
     } finally {
         await client.close(); 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,18 @@
 import express, { Request, Response } from 'express';
-import { readAdmin} from './CRUD/users/admin/getadmin';
+import { readAdmin } from './CRUD/users/admin/getadmin';
 import { updateAdmin } from './CRUD/users/admin/updateadmin';
 import { createUser } from './CRUD/users/classicuser/createuser';
 import { deleteUser } from './CRUD/users/classicuser/deleteuser';
-import { getUser } from './CRUD/users/classicuser/getuser';
+import { getUserById, getUsers } from './CRUD/users/classicuser/getuser';
 import { createAdmin } from './CRUD/users/admin/createadmin';
+import { updateUser } from './CRUD/users/classicuser/updateuser';
+
+
 const app = express();
 app.use(express.json());
 
-app.get("/read-admin/:id", async (req: Request, res: Response) => {
+// Admin routes
+app.get("/admin/read/:id", async (req: Request, res: Response) => {
   try {
     const id = req.params.id;
     const admins = await readAdmin(id);
@@ -18,7 +22,7 @@ app.get("/read-admin/:id", async (req: Request, res: Response) => {
   }
 });
 
-app.put("/update-admin/:id", async (req: Request, res: Response) => {
+app.put("/admin/update/:id", async (req: Request, res: Response) => {
   try {
     const email = req.params.email;
     const name = req.params.name;
@@ -30,32 +34,7 @@ app.put("/update-admin/:id", async (req: Request, res: Response) => {
   }
 });
 
-app.post("/create-user", async (req: Request, res: Response) => {
-  try {
-    const email = req.params.email;
-    const name = req.params.name;
-    const password = req.params.password;
-    await createUser(email, name, password, true);
-    res.send("User created successfully");
-  } catch (error: any) {
-    res.status(500).send("Failed to create user: " + error.message);
-  }
-});
-
-app.delete("/delete-user/:id", async (req: Request, res: Response) => {
-  try {
-    const id = req.params.id;
-    await deleteUser(id);
-    res.send("User deleted successfully");
-  } catch (error: any) {
-    res.status(500).send("Failed to delete user: " + error.message);
-  }
-});
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
-app.post("/create-admin", async (req: Request, res: Response) => {
+app.post("/admin/create", async (req: Request, res: Response) => {
   try {
     const email = req.body.email;
     const name = req.body.name;
@@ -67,35 +46,31 @@ app.post("/create-admin", async (req: Request, res: Response) => {
   }
 });
 
-app.get("/read-user/:id", async (req: Request, res: Response) => {
+// User routes
+app.get("/user/read/:id", async (req: Request, res: Response) => {
   try {
     const id = req.params.id;
-    const user = await getUser(id);
+    const user = await getUserById(id);
     res.send(user);
   } catch (error: any) {
     res.status(500).send("Failed to read user: " + error.message);
   }
 });
 
-
-
-app.post("/create-admin", async (req: Request, res: Response) => {
+app.get("/user/read", async (req: Request, res: Response) => {
   try {
-    const email = 'testadmin@gmail.com';
-    const name = 'admin';
-    const password = '1';
-    await createAdmin(email, name, password, true);
-    res.send("Admin created successfully");
+    const users = await getUsers();
+    res.send(users);
   } catch (error: any) {
-    res.status(500).send("Failed to create admin: " + error.message);
+    res.status(500).send("Failed to read users: " + error.message);
   }
 });
 
-app.post("/create-user", async (req: Request, res: Response) => {
+app.post("/user/create", async (req: Request, res: Response) => {
   try {
-    const email = 'testuser@gmail.com';
-    const name = 'admin';
-    const password = '2';
+    const email = req.body.email;
+    const name = req.body.name;
+    const password = req.body.password;
     await createUser(email, name, password, true);
     res.send("User created successfully");
   } catch (error: any) {
@@ -103,26 +78,27 @@ app.post("/create-user", async (req: Request, res: Response) => {
   }
 });
 
-app.get("/create-admin-test", async (req: Request, res: Response) => {
+app.delete("/user/delete/:id", async (req: Request, res: Response) => {
   try {
-    const email = 'testadmin@gmail.com';
-    const name = 'admin';
-    const password = '1';
-    await createAdmin(email, name, password, true);
-    res.send("Admin created successfully");
+    const id = req.params.id;
+    await deleteUser(id);
+    res.send("User deleted successfully");
   } catch (error: any) {
-    res.status(500).send("Failed to create admin: " + error.message);
+    res.status(500).send("Failed to delete user: " + error.message);
   }
 });
-
-app.get("/create-user-test", async (req: Request, res: Response) => {
+app.put("/user/update/:id", async (req: Request, res: Response) => {
   try {
-    const email = 'testuser@gmail.com';
-    const name = 'admin';
-    const password = '2';
-    await createUser(email, name, password, true);
-    res.send("User created successfully");
+    const id = req.params.id;
+    const email = req.body.email;
+    const name = req.body.name;
+    const password = req.body.password;
+    await updateUser(id, email, name, password);
+    res.send("User updated successfully");
   } catch (error: any) {
-    res.status(500).send("Failed to create user: " + error.message);
+    res.status(500).send("Failed to update user: " + error.message);
   }
+});
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
