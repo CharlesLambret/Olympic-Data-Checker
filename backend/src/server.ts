04bfcb45429da905ffad57a4249982c9dc8e9ad7
toutouff <63313022+toutouff@users.noreply.github.com>
2024-04-19 11:01:52 +0200

    server reparé

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,5 +8,5 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.listen(port, () => {
-  console.log(`[Server]: I am running at https://localhost:${port}`);
+  console.log(`[Server]: I am running at http://localhost:${port}`);
 });
