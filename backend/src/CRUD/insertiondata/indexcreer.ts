f2d50024118f31aca7fa33e5db3441bd98c18fc1
Charles Lambret <charleslambretpro@gmail.com>
2024-05-14 17:14:17 +0200

    décompilation js to ts

--- backend/src/CRUD/insertiondata/indexcreer.ts
+++ backend/src/CRUD/insertiondata/indexcreer.ts
@@ -1,15 +1,11 @@
 import { Router } from 'express';
-import creerjeux from './insertGames';
 import creerathlete from './insertAthletes';
-import {creerevenements} from './insertionévénements';
 import creermedailles from './insertmedals';
 
 const indexinsertion = Router();
 
 indexinsertion.use(
     creerathlete   ,
-    creerjeux,
-    creerevenements,
     creermedailles
 );
 
