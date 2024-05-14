43dee8f91b309d738b36b6c89c5c7b40184ca439
Charles Lambret <charleslambretpro@gmail.com>
2024-05-14 17:11:42 +0200

    décompilation js to ts

---
+++ backend/src/CRUD/insertiondata/interfaces.ts
@@ -0,0 +1,40 @@
+import { ObjectId } from "mongodb";
+
+export interface Athlete {
+    _id: ObjectId;
+    Nom: string;
+    Discipline: string;
+    Age: number;
+    Poids: number;
+    Taille: number;
+    Sexe: 'M' | 'F';
+    PaysID: ObjectId;
+}
+
+export interface Event {
+    _id : ObjectId;
+    Discipline: string;
+    NomEvent: string;
+    JeuxID: ObjectId;
+}
+
+export interface Medal {
+    _id: ObjectId;
+    AthleteID: ObjectId;
+    EventID: ObjectId;
+    NomMedaille : 'Or' | 'Argent' | 'Bronze';
+}
+
+export interface Country {
+    _id : ObjectId;
+    noc : string;
+    region : string;
+    notes : string;
+}
+
+export interface Game {
+    _id : ObjectId;
+    Annee : number;
+    Saison : 'Summer' | 'Winter';
+    Ville : string;
+}
