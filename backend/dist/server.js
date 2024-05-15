64545e29eec603f9c4d59555c4916de632b2d79e
Charles Lambret <charleslambretpro@gmail.com>
2024-05-15 15:04:21 +0200

    fix websocket et middleware

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -4,10 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const express_session_1 = __importDefault(require("express-session"));
 const classicuser_1 = __importDefault(require("./CRUD/users/classicuser"));
 const indexusers_1 = __importDefault(require("./CRUD/users/admin/indexusers"));
-const express_session_1 = __importDefault(require("express-session"));
 const indexolympics_1 = __importDefault(require("./CRUD/olympics/indexolympics"));
+const http_1 = require("http");
+const ws_1 = require("ws");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, express_session_1.default)({
@@ -17,7 +19,20 @@ app.use((0, express_session_1.default)({
     name: 'connect.sid',
     cookie: { secure: 'auto', httpOnly: true }
 }));
+let connections = [];
+const server = (0, http_1.createServer)(app);
+const wss = new ws_1.Server({ server });
+wss.on('connection', (ws) => {
+    connections.push(ws);
+    ws.on('message', (message) => {
+        ws.send('Message reçu');
+    });
+    ws.on('close', () => {
+        connections = connections.filter(conn => conn !== ws);
+    });
+    ws.send('Bienvenue sur le serveur WebSocket');
+});
 app.use(classicuser_1.default, indexusers_1.default, indexolympics_1.default);
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,18 @@
 import express from 'express';
+import session from 'express-session';
+
 import indexuser from './CRUD/users/classicuser';
 import indexadmin from './CRUD/users/admin/indexusers';
-import session from 'express-session';
 import indexolympics from './CRUD/olympics/indexolympics';
 
+import { createServer } from 'http';
+import { Server, WebSocket } from 'ws';
+
+// ---------------- Init serveur ---------------- //
 const app = express();
 app.use(express.json());
 
+// ---------------- Session ---------------- //
 app.use(session({
   secret: '8xxR1ZXfXUMKYqcsdhCU',
   resave: false,
@@ -15,13 +21,38 @@ app.use(session({
   cookie: { secure: 'auto', httpOnly: true }
 }));
 
+// ---------------- Web socket ---------------- //
+
+let connections: any[] = [];
+
+const server = createServer(app);
+
+const wss = new Server({ server });
+
+wss.on('connection', (ws: WebSocket) => {
+  
+  connections.push(ws);
+
+  ws.on('message', (message: string) => {
+    ws.send('Message reçu');
+  });
+
+  ws.on('close', () => {
+    connections = connections.filter(conn => conn !== ws);
+  });
+
+  ws.send('Bienvenue sur le serveur WebSocket');
+});
+
+// ---------------- Routes ---------------- //
 app.use(
   indexuser, 
   indexadmin, 
   indexolympics
 );
 
+// ---------------- Confirmation ---------------- //
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
