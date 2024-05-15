dc66656ef48a983cd741682ef5a3b04ed4c33750
Charles Lambret <charleslambretpro@gmail.com>
2024-05-15 14:52:34 +0200

    fix login

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,13 +7,13 @@ import indexolympics from './CRUD/olympics/indexolympics';
 const app = express();
 app.use(express.json());
 
-// app.use(session({
-//   secret: '8xxR1ZXfXUMKYqcsdhCU',
-//   resave: false,
-//   saveUninitialized: false,
-//   name: 'connect.sid',
-//   cookie: { secure: 'auto', httpOnly: true }
-// }));
+app.use(session({
+  secret: '8xxR1ZXfXUMKYqcsdhCU',
+  resave: false,
+  saveUninitialized: false,
+  name: 'connect.sid',
+  cookie: { secure: 'auto', httpOnly: true }
+}));
 
 app.use(
   indexuser, 
