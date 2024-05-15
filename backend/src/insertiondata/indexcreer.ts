47c5417cff9379cf1e47322a890464628c0c0507
Charles Lambret <charleslambretpro@gmail.com>
2024-05-15 13:58:53 +0200

    correction code total medailles

--- backend/src/insertiondata/indexcreer.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import insertAthletes from './insertAthletes';
-import insertmedals from './insertmedals';
-
-const indexinsertion = Router();
-
-indexinsertion.use(insertAthletes, insertmedals);
-
-export default indexinsertion;
