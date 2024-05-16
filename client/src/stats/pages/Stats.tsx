51b02a073914560708c3d30fe294e272e1b2ff84
Paul MAZEAU <mazeau.paul@gmail.com>
2024-05-16 15:20:46 +0200

    gros push salllle

--- client/src/stats/pages/Stats.tsx
+++ client/src/stats/pages/Stats.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { CardsStats } from "../components/CardStats";
-import { TableMedals } from "../components/TableMedals";
 
 const Stats: React.FC = () => {
   return (
